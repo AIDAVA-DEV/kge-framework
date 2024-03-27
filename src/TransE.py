4d36f2712e1e56a1b8d0af67b7e7b9966466632a
shervin <shervin_mehryar@hotmail.com>
2024-03-27 15:01:47 +0100

    TransE workflow added for NodePredcition.

--- src/TransE.py
+++ src/TransE.py
@@ -1,24 +1,124 @@
-from    models  import BaseModel
+from    src.models  import BaseModel as bm
 from    torch   import nn
 import  torch
 
+from    src.loaders import TestDataset, TrainDataset
+from    torch.utils.data import DataLoader
+
+import  numpy as np
+
 class TransE(nn.Module):
     
-    def __init__(self,num_entities,num_relations,device,emb_dim=30, type_count=1,lr=1e-3):
+    def __init__(self,num_entities,num_relations,device,emb_dim=30,lr=1e-3):
         super(TransE,self).__init__()
         
         self.device = device
         
         self.num_entities = num_entities
         
-        self.model        = BaseModel(num_entities,num_relations,emb_dim,device)
+        self.model        = bm(num_entities,num_relations,emb_dim).to(device)
 
         params = list()
         params.append(self.model.entity_embds )
         params.append(self.model.rel_embds )
         
         """ ref: https://stackoverflow.com/questions/72679858/cant-optimize-a-non-leaf-tensor-on-torch-parameter """
+        
         self.optimizer    = torch.optim.Adam(params,lr=lr)
+
+        # initialize embeddings with random normal values
+        self.entity_embds = nn.Parameter(torch.randn(num_entities,emb_dim))
+        self.rel_embds    = nn.Parameter(torch.randn(num_relations,emb_dim))
+    
+    def _train(self , train_triples,
+               train_batch_size = 32 ,
+               num_epochs      = 100):
+
+        train_db = TrainDataset(train_triples,self.num_entities,filter=False)
+        train_dl = DataLoader(train_db,batch_size=train_batch_size,shuffle=True)
+        
+
+        log_freq = num_epochs//10
+        
+        train_losses = []
+        for e in range(num_epochs):  
+
+            self.model.train()
+            losses = []
+
+            for batch in train_dl:
+
+                self.optimizer.zero_grad()
+
+                # late binding to specific model via compute_loss
+                loss = self.compute_loss(batch)
+
+                loss.backward()
+                self.optimizer.step()
+                
+                if np.isnan(loss.item()):
+                    print('in _train: found invalid loss value, NaN')
+                else:
+                    losses.append(loss.item())
+
+            if e % log_freq == 0:
+                if len(losses)!=0:
+                    mean_loss = np.array(losses).mean()
+                    print('epoch {},\t train loss {:0.02f}'.format(e,mean_loss))
+                else:
+                    mean_loss = np.NaN
+                    print('in _train: found invalid mean loss, NaN')
+                
+            train_losses.append(mean_loss)
+
+        return train_losses
+    
+    def _eval(self, eval_triples):
+        
+        self.model.eval() 
+        self.model.to(self.device)
+
+        test_db = TestDataset(eval_triples,self.num_entities,filter=False)
+        test_dl = DataLoader(test_db,batch_size=len(test_db),shuffle=False)
+
+        # load all
+        batch = next(iter(test_dl))
+        
+        edges, edge_rels            = batch
+        batch_size, num_samples, _  = edges.size()
+        edges                       = edges.view(batch_size*num_samples,-1)
+        edge_rels                   = edge_rels.view(batch_size*num_samples,-1)
+
+        h_indx = torch.tensor([int(x) for x in edges[:,0]],device=self.device)
+        r_indx = torch.tensor([int(x) for x in edge_rels.squeeze()],device=self.device)
+        t_indx = torch.tensor([int(x) for x in edges[:,1]],device=self.device)
+        
+        scores = self.predict(h_indx,r_indx,t_indx,batch_size,num_samples)
+
+        # sort and calculate scores
+        argsort   = torch.argsort(scores,dim = 1,descending= False)
+        rank_list = torch.nonzero(argsort==0,as_tuple=False)
+        rank_list = rank_list[:,1] + 1
+        
+        hits1_list  = []
+        hits10_list = []
+        MR_list     = []
+        MRR_list    = []
+  
+        hits1_list.append( (rank_list <= 1).to(torch.float).mean() )
+        hits10_list.append( (rank_list <= 10).to(torch.float).mean() )
+        MR_list.append(rank_list.to(torch.float).mean())
+        MRR_list.append( (1./rank_list.to(torch.float)).mean() )
+  
+        hits1   = sum(hits1_list)/len(hits1_list)
+        hits10  = sum(hits10_list)/len(hits10_list)
+        mr      = sum(MR_list)/len(MR_list)
+        mrr     = sum(MRR_list)/len(MRR_list)
+
+        print(f'hits@1 {hits1.item():0.02f} hits@10 {hits10.item():0.02f}',
+              f' MR {mr.item():0.02f} MRR  {mrr.item():0.02f}')
+    
+
 
     def predict(self,batch_h,batch_r ,batch_t,batch_size,num_samples):
 
